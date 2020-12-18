a1e765d6501f94488442a7816d49db3fff080b72
MRsLl <33240946+MRsLl@users.noreply.github.com>
2020-12-18 09:13:05 +0800

    Update PmsOssController.java

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/PmsOssController.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/PmsOssController.java
@@ -21,10 +21,10 @@ import java.util.Map;
 @RestController
 public class PmsOssController {
 
-    String accessId = "LTAI4G7h4Ujt8zoQL2ixMavW"; // 请填写您的AccessKeyId。
-    String accessKey = "VskFsL0TCFaaGzrGsDPTVhOyFqbLdt"; // 请填写您的AccessKeySecret。
+    String accessId = // 请填写您的AccessKeyId。
+    String accessKey = // 请填写您的AccessKeySecret。
     String endpoint = "oss-cn-shanghai.aliyuncs.com"; // 请填写您的 endpoint。
-    String bucket = "gmall-mrsl"; // 请填写您的 bucketname 。
+    String bucket =  // 请填写您的 bucketname 。
     String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
     // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
     //String callbackUrl = "http://88.88.88.88:8888";
